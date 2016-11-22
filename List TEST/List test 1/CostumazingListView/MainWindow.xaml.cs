a38256276587404645166ac53ac4b266c04860ac
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2016-11-23 00:14:56 +0800

    ListViwe con boton e imagne
    Se crea una lista personalizada con un boton (sin evento aun) y una
    imagen para diferenciar la operacion.

--- List TEST/List test 1/CostumazingListView/MainWindow.xaml.cs
+++ List TEST/List test 1/CostumazingListView/MainWindow.xaml.cs
@@ -20,9 +20,39 @@ namespace CostumazingListView
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Operation> currentList;
         public MainWindow()
         {
             InitializeComponent();
+            this.currentList = new List<Operation>();
+            this.listViewOperations.ItemsSource = null;
+        }
+
+        private void buttonOperation1_Click(object sender, RoutedEventArgs e)
+        {
+            this.currentList.Add(new Operation
+            {
+                Type = Operation.externalTurning,
+                Index = DateTime.Now.ToString("hhmmss"),
+                OperationType = operations.External_Turning,
+                Parameters = "Z=150 XF=25.25 F125.25 Z2.5 S=12.5 Mx=12.45 TF=125.8"
+            });
+            this.listViewOperations.ItemsSource = null;
+            this.listViewOperations.ItemsSource = currentList;
+        }
+
+        private void buttonOperation2_Click(object sender, RoutedEventArgs e)
+        {
+            this.currentList.Add(new Operation
+            {
+                Type = Operation.internalTurning,
+                Index = DateTime.Now.ToString("hhmmss"),
+                OperationType = operations.Internal_Turning,
+                Parameters = "Z=150 XF=25.25 F125.25 Z2.5 S=12.5 Mx=12.45 TF=125.8"
+            });
+            this.listViewOperations.ItemsSource = null;
+            this.listViewOperations.ItemsSource = currentList;
         }
     }
-}
+    }
+
---
+++ List TEST/List test 1/CostumazingListView/Operation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CostumazingListView
+{
+    class Operation
+    {
+        public const string externalTurning = "/Image/externalTurning.png";
+        public const string internalTurning = "/Image/internalTurning.png";
+
+        public string Type { get; set; }
+        public string Index { get; set; }
+        public operations OperationType { get; set; }
+        public string Parameters { get; set; }
+
+
+    }
+    public enum operations
+    {
+        External_Turning,
+        Internal_Turning,
+    }
+}
